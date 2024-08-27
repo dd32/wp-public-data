3d73a81ba88dfc756b83d2049753016e0f38c325
Dion Hulse <dion@wordpress.org>
2024-08-27 11:18:06 +0000

    Request all fields, store downloads even though it's often outdated.

--- bin/update-plugins.php
+++ bin/update-plugins.php
@@ -1,10 +1,8 @@
 <?php
 /**
  * Updates the plugin .json files by refreshing the plugins based on the last days worth of updated plugins.
- * This is not 100% accurate, in that some data within the .json will be outdated, such as the download count.
  *
  * TODO:
- *  - Remove Download count from the .jsons?
  *  - Use a proper user agent
  */
 
@@ -23,15 +21,15 @@ do {
 	foreach ( $plugins->plugins as $plugin_data ) {
 		$updated_timestamp = strtotime( $plugin_data->last_updated );
 		if ( $updated_timestamp < time() - 60 * 60 * 12 ) {
-			break 2; // Stop processing if we hit a plugin that hasn't been updated in the last 12hrs
+			break 2; // Stop processing if we hit a plugin that hasn't been updated in the 12hrs
 		}
 
 		$slug = $plugin_data->slug;
 
-		fwrite( STDERR, "\t$slug\n" );
+		fwrite( STDERR, "\t$slug {$plugin_data->version} ({$plugin_data->last_updated})\n" );
 
 		// Fetch full plugin data.
-		$data = json_decode( file_get_contents( 'https://api.wordpress.org/plugins/info/1.2/?action=plugin_information&fields[]=language_packs&slug=' . $slug ), true );
+		$data = json_decode( file_get_contents( 'https://api.wordpress.org/plugins/info/1.2/?action=plugin_information&fields=language_packs,compatibility,short_description,description,icons,blocks,block_assets,author_block_count,author_block_rating,blueprints,stable_tag,downloaded&slug=' . $slug ), true );
 
 		if ( ! $data ) {
 			$data = $plugin_data; // Partial. meh.
@@ -39,6 +37,9 @@ do {
 
 		// Cleanup.. We're not storing reviews or duplicated screenshot HTML here.
 		unset( $data['sections']['reviews'], $data['sections']['screenshots'] );
+
+		// Ensure screenshots are always an array.
+		$data['screenshots'] = array_values( $data['screenshots'] );
 
 		file_put_contents( dirname( __DIR__ ) . '/plugins/' . $slug . '.json', json_encode( $data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE ) );
 		usleep( 250000 ); // 250ms Slow we go.
