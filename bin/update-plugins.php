cf9ab365d8e90fce2dca177d8066efc934ea96c7
dd32 <dd32@users.noreply.github.com>
2024-08-27 10:08:38 +0000

    Add Plugins

---
+++ bin/update-plugins.php
@@ -0,0 +1,40 @@
+<?php
+
+$per_page = 1;
+do {
+	fwrite( STDERR, "Processing page $per_page\n" );
+
+	$url = 'https://api.wordpress.org/plugins/info/1.2/?action=query_plugins&browse=updated&posts_per_page=100&paged=' . $per_page++;
+
+	$plugins = json_decode( file_get_contents( $url ) );
+	if ( ! $plugins || ! $plugins->plugins ) {
+		break;
+	}
+
+	foreach ( $plugins->plugins as $plugin_data ) {
+		$updated_timestamp = strtotime( $plugin_data->last_updated );
+		if ( $updated_timestamp < time() - 60 * 60 * 24 ) {
+			break 2; // Stop processing if we hit a plugin that hasn't been updated in the last day
+		}
+
+		$slug = $plugin_data->slug;
+
+		fwrite( STDERR, "\t$slug\n" );
+
+		// Fetch full plugin data.
+		$data = json_decode( file_get_contents( 'https://api.wordpress.org/plugins/info/1.2/?action=plugin_information&fields[]=language_packs&slug=' . $slug ) );
+
+		if ( ! $data ) {
+			$data = $plugin_data; // Partial. meh.
+		}
+
+		// Cleanup.. We're not storing reviews or duplicated screenshot HTML here.
+		unset( $data['sections']['reviews'], $data['sections']['screenshots'] );
+
+		file_put_contents( dirname( __DIR__ ) . '/plugins/' . $slug . '.json', json_encode( $data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE ) );
+		usleep( 250000 ); // 250ms Slow we go.
+	}
+
+} while( true );
+
+echo "Done.";
