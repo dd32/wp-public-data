1565c433852dbd132385a1c2af846f787034fd70
Dion Hulse <contact+github@dd32.id.au>
2024-08-28 13:57:10 +1000

    Resolve PHP Warning, non-numeric value `intval( '5m' )`

--- bin/update-plugins.php
+++ bin/update-plugins.php
@@ -26,10 +26,11 @@ do {
 
 		$slug = $plugin_data->slug;
 
-		$mins_ago = intval( ( time() - $updated_timestamp ) / 60 ) . 'm';
+		$mins_ago = intval( ( time() - $updated_timestamp ) / 60 );
 		if ( intval( $mins_ago ) > 60 ) {
-			$mins_ago = intval( $mins_ago / 60 ) . 'h ' . ( $mins_ago % 60 ) . 'm';
+			$mins_ago = intval( $mins_ago / 60 ) . 'h ' . ( $mins_ago % 60 );
 		}
+		$mins_ago .= 'm';
 
 		fwrite( STDERR, "\t$slug {$plugin_data->version} (updated {$mins_ago} ago)\n" );
 
